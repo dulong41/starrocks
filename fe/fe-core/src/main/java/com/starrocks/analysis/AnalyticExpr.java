54685f5255563c8f8a532b09edd62bebb71025ac
Stephen <dulong41@gmail.com>
2022-11-10 15:52:43 +0800

    Update AnalyticExpr.java

--- fe/fe-core/src/main/java/com/starrocks/analysis/AnalyticExpr.java
+++ fe/fe-core/src/main/java/com/starrocks/analysis/AnalyticExpr.java
@@ -32,6 +32,7 @@ import com.starrocks.common.AnalysisException;
 import com.starrocks.sql.ast.AstVisitor;
 import com.starrocks.thrift.TExprNode;
 import com.starrocks.thrift.TExprNode;
+import com.starrocks.thrift.TExprNode;
 
 import java.util.ArrayList;
 import java.util.List;
